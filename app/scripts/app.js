43bcc5b2501bc38f90d520bbf110665c07255d7e
Lanzer <ernestlanza@gmail.com>
2016-10-07 14:24:33 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularjsFundamentalsApp
+ * @description
+ * # angularjsFundamentalsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularjsFundamentalsApp', [
+    'ngAnimate'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsFundamentalsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsFundamentalsApp
+ */
+angular.module('angularjsFundamentalsApp')
+  .controller('MainCtrl', function () {
+
+  });
