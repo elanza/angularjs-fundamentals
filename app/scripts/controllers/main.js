a76bbc54fe9088b8319e070cc2003c4c7c644a38
Lanzer <ernestlanza@gmail.com>
2016-10-08 10:37:31 -0400

    test
    comment test

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,5 +9,10 @@
  */
 angular.module('angularjsFundamentalsApp')
   .controller('MainCtrl', function () {
-
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    this.name = "Name of controller";
   });
